9b49d8aad50808e076a811f14bfbbd5e60fa6082
Dmitry Branitskiy <dmitry.branitskiy@gmail.com>
2020-04-27 21:33:20 -0400

    Adding visitor pattern

--- index.js
+++ index.js
@@ -6,9 +6,21 @@ document.addEventListener('DOMContentLoaded', init);
 function init() {
   const input = document.getElementById('input');
   const output = document.getElementById('output');
-  input.addEventListener('change', (e) => {
-    const body = parse(e.target.value).body;
-    new Interpreter(body);
-    output.textContent = body;
+  input.addEventListener('keyup', (e) => {
+    new Promise((res, rej) => {
+      const body = parse(e.target.value).body;
+      res(new Interpreter(body));
+    }).then((body) => {
+      output.textContent = body;
+    }).catch((e) => {
+      output.textContent = e;
+    });
   });
+
+window.addEventListener('unhandledrejection', function(event) {
+  // prevent Default debug logging
+  event.preventDefault();
+  console.warn(event.promise); // [object Promise] - the promise that generated the error
+  console.warn(event.reason); // Error: Whoops! - the unhandled error object
+});
 }
--- interpreter.js
+++ interpreter.js
@@ -1,6 +1,31 @@
 class Interpreter {
   constructor(ast) {
-    console.log(ast);
+    this.traverse(ast);
+  }
+  traverse(ast) {
+    ast.forEach((el) => {
+      let astEl = astTypeFactory(el);
+      astEl.visit();
+    });
+  }
+  evaluate(el) {
+  }
+}
+
+function astTypeFactory(el) {
+  switch(el.type) {
+    case "FunctionDeclaration":
+      return new FunctionDeclaration(el)
+      break;
+  }
+}
+
+class FunctionDeclaration {
+  constructor(el) {
+    Object.assign(this, el);
+  }
+  visit() {
+    console.info(this);
   }
 }
 
